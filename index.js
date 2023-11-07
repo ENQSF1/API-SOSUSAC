ac3bb442f36c14ee23fb7d76212e1ec016573b39
kevin hernandez <kevinhernandz15@gmail.com>
2023-11-06 23:38:05 -0600

    intento 4

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const app = express();
 const morgan = require("morgan");
 app.set("port", process.env.PORT || 3000);
 
+const candidatos = require("./routes/candidatos");
+const valores = require("./routes/valores");
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json("hola mundo");
+});
+
 //routes
 
-app.use("/api/candidatos", require("./routes/candidatos"));
-app.use("/api/valores", require("./routes/valores"));
+app.use("/api/candidatos", candidatos);
+app.use("/api/valores", valores);
 
 app.listen(app.get("port"), () => {
   console.log(`server on port ${app.get("port")}`);
